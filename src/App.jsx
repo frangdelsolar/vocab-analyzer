cb877108f588dd36760ecbe7569b97f6e8491559
Francisco Gonzalez del Solar <frangdelsolar@gmail.com>
2026-01-13 20:41:00 +0800

    insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { VocabularyProvider } from './contexts/VocabularyContext';
 import { DialoguesProvider } from './contexts/DialoguesContext';
 import VocabularyTable from './components/VocabularyTable';
 import VocabularySettings from './components/VocabularySettings';
+import VocabularyInsights from './components/VocabularyInsights'; // Add this import
 import DialoguesList from './components/DialoguesList';
 import Layout from './components/Layout';
 
@@ -15,6 +16,7 @@ function App() {
                 </Layout>
                 <Layout>
                     <VocabularySettings />
+                    <VocabularyInsights /> {/* Add this component */}
                     <VocabularyTable />
                 </Layout>
             </DialoguesProvider>
